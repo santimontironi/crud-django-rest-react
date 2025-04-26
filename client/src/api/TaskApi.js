a2056605dc12ec8785ba854691c0541abf82afd5
santimontironi <santiimontironi@gmail.com>
2025-04-26 00:36:06 -0300

    se agrego el metodo post

--- client/src/api/TaskApi.js
+++ client/src/api/TaskApi.js
@@ -1,5 +1,13 @@
 import axios from 'axios'
 
+const config = axios.create({
+    baseURL:'http://localhost:8000/tasks/api/tasks/'
+})
+
 export const getAllTasks = () => {
-    return axios.get('http://localhost:8000/tasks/api/tasks/')
+    return config.get('/') //la barra '/' hace referencia a baseURL
+}
+
+export const sendTask = (task) =>{
+    return config.post('/',task)
 }
