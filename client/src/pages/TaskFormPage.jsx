a4ca95941e237a90f38dfa2d27d9d0dcbe3cbc1c
santimontironi <santiimontironi@gmail.com>
2025-04-26 00:17:37 -0300

    se aplicacion los errores al enviar el formulario

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -2,19 +2,24 @@ import { useForm } from "react-hook-form"; //este hook da todas las herramientas
 
 export const TaskFormPage = () => {
 
-  const { register, handleSubmit } = useForm() //register conecta cada input al sistema de React Hook Form (sabe su valor, cuándo cambia, etc.)
-  
-    //esta es la funcion que tiene handleSubmit como parametro.
-    function onSubmit(data){
-      console.log(data)
-    }
+  const { register, handleSubmit, formState: { errors } } = useForm() //register conecta cada input al sistema de React Hook Form (sabe su valor, cuándo cambia, etc.)
 
+  //esta es la funcion que tiene handleSubmit como parametro.
+  function onSubmit(data) {
+    console.log(data)
+  }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type="text" placeholder="title" {...register("title", { required: true })} />
+      {errors.title && (
+        <p>This title is required</p>
+      )}
       <textarea rows="3" placeholder="Description" {...register("description", { required: true })}></textarea>
       <button>Save</button>
+      {errors.description && (
+        <p>This textarea is required</p>
+      )}
     </form>
   )
 }
