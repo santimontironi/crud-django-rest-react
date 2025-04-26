01ed727608cfb84a3d1474fdf3da9ead12608e50
santimontironi <santiimontironi@gmail.com>
2025-04-26 00:11:43 -0300

    comentarios y funcionalidades agregadas en el formulario

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,11 +1,17 @@
-import { useForm } from "react-hook-form";
+import { useForm } from "react-hook-form"; //este hook da todas las herramientas para manejar un formulario de forma más sencilla, sin tener que usar useState para cada campo ni escribir validaciones a mano.
 
 export const TaskFormPage = () => {
 
-  const { register } = useForm() //register es para registrar inputs
+  const { register, handleSubmit } = useForm() //register conecta cada input al sistema de React Hook Form (sabe su valor, cuándo cambia, etc.)
+  
+    //esta es la funcion que tiene handleSubmit como parametro.
+    function onSubmit(data){
+      console.log(data)
+    }
+
 
   return (
-    <form action="">
+    <form onSubmit={handleSubmit(onSubmit)}>
       <input type="text" placeholder="title" {...register("title", { required: true })} />
       <textarea rows="3" placeholder="Description" {...register("description", { required: true })}></textarea>
       <button>Save</button>
